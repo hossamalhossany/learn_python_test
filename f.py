b7fefdd81f912c83294a24d21594452ea48932e8
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-03-23 11:47:00 +0200

    632

--- f.py
+++ f.py
@@ -1,2 +1,23 @@
-class Car:
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
     
+    def print_data(self):
+        print(f'the person {self.name} has age ===  {self.age}')
+
+
+
+
+# p1 = Person('hossam',44)
+
+
+class Person_child(Person):
+    def address(self,address):
+        print(f'the person live in address {address}')
+
+p1 = Person_child('hossam',55)
+
+
+p1.print_data()
+p1.address('giza')    
