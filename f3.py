096440950b64c4702e7702921874eca781236f1d
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-07-26 11:25:00 +0200

    26-7 ___1

---
+++ f3.py
@@ -0,0 +1,13 @@
+a= [0,1,2,3]
+
+for a[-1] in a :
+    print(a[-1])
+
+
+
+
+# Correct output is 0,1,2,2 
+
+#  a[-1] is pointing to last position of the list. 
+#  So all the values of list be assigned to last position of the list one by one. 
+#  When it comes to 4th iteration the value of last position is already 2.
